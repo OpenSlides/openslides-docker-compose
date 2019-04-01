9985026a16f1e7a0d0e0f4436dd950bbfc83a5aa
Gernot Schulz <gernot@intevation.de>
2019-04-01 15:32:37 +0200

    bash completion: update for new and renamed options

--- contrib/bash-completion.sh
+++ contrib/bash-completion.sh
@@ -4,9 +4,10 @@ _osinstancectl()
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="ls add rm --help --verbose --online --offline --no-add-account
-    --revision --repo --clone-from"
-  diropts="ls|rm|--clone-from"
+  opts="ls add rm start stop update erase"
+  opts+="--help --long --metadata --online --offline --no-add-account"
+  opts+="--revision --repo --clone-from --force --color --project-dir"
+  diropts="ls|rm|start|stop|update|erase|--clone-from"
 
   if [[ ${prev} =~ ${diropts} ]]; then
     COMPREPLY=( $(cd /srv/openslides/docker-instances && compgen -d -- ${cur}) )
