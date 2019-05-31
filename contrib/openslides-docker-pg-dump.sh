c7718f27192c5acf58bfc4693321ea1b5b58b51a
Gernot Schulz <gernot@intevation.de>
2019-05-31 15:12:05 +0200

    openslides-docker-pg-dump: Configurable output dir

--- contrib/openslides-docker-pg-dump.sh
+++ contrib/openslides-docker-pg-dump.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-# Iterate over all OpenSlides Postgres Docker containers and generate SQL
-# dumps.  Removal of dumps should be handled by an external tool, e.g., the
-# bup-backup backup script.
-
 set -euo pipefail
 umask 0027
 
-NAME_FILTER="${1-.}"
-
 BACKUP_PATH="/backup/docker-sql-dumps"
+NAME_FILTER="."
+ME=$(basename -s .sh "${BASH_SOURCE[0]}")
+
+usage() {
+cat <<EOF
+Usage: ${BASH_SOURCE[0]} [options] [<container name filter>]
+
+Create SQL dumps from OpenSlides dockerized Postgres DBs.  Containers can be
+specified using their names.  Without arguments, the script iterates over all
+OpenSlides database containers!
+
+Removal of dumps should be handled by an external tool.  The dumps are not
+compressed to allow for more effective deduplication by backup tools.
+
+Options:
+  -o, --output-dir   Specify output directory for SQL dumps
+                     (default: $BACKUP_PATH)
+EOF
+}
+
+shortopt="ho:"
+longopt="help,output-dir:"
+
+ARGS=$(getopt -o "$shortopt" -l "$longopt" -n "$ME" -- "$@")
+if [ $? -ne 0 ]; then usage; exit 1; fi
+eval set -- "$ARGS";
+unset ARGS
+
+# Parse options
+while true; do
+  case "$1" in
+    -o|--output-dir)
+      BACKUP_PATH="$2"
+      shift 2
+      ;;
+    -h|--help) usage; exit 0 ;;
+    --) shift ; break ;;
+    *) usage; exit 1 ;;
+  esac
+done
+
 [[ -d "$BACKUP_PATH" ]] || { echo "ERROR: $BACKUP_PATH not found!"; exit 3; }
+
+NAME_FILTER="$*"
 
 docker ps --filter=name="_postgres_" --format "{{.ID}} {{.Names}} {{.Labels}}" |
 while read id name labels; do
