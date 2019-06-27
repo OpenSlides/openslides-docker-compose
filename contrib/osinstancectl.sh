446c8f48c11c93351a24f66e2a9f80ef3fdc378b
Gernot Schulz <gernot@intevation.de>
2019-06-27 13:00:13 +0200

    Setup: Use same Django secret key across nodes
    Upon setup, Django generates a random secret to for its settings.py.
    Due to the shared volume, this secret could be shared amongst multiple
    server containers, e.g., the server and prioserver services.  However,
    in case of Docker Swarm a container might be started on a new node.
    Here, Django would create a new, random secret key.  This would lead to
    authentication failures as requests are load balanced across the Swarm
    nodes.
    
    This patch generates a single secret per instance, similar to the user
    account credentials.  Using docker-stack.yml, the secret is shared
    within the swarm, so containers on new nodes will operate with the same
    secret key.

--- contrib/osinstancectl.sh
+++ contrib/osinstancectl.sh
@@ -268,6 +268,14 @@ gen_pw() {
   echo "$PW"
 }
 
+create_django_secrets_file() {
+  echo "Generating Django secret key (for settings.py)..."
+  [[ -d "${PROJECT_DIR}/secrets" ]] ||
+    mkdir -m 700 "${PROJECT_DIR}/secrets"
+  printf "%s\n" "$(gen_pw)" \
+    >> "${PROJECT_DIR}/secrets/django"
+}
+
 create_admin_secrets_file() {
   echo "Generating admin password..."
   [[ -d "${PROJECT_DIR}/secrets" ]] ||
@@ -985,6 +993,7 @@ case "$MODE" in
     create_instance_dir
     create_config_from_template "${PROJECT_DIR}/docker-compose.yml.example" \
       "${PROJECT_DIR}/docker-compose.yml"
+    create_django_secrets_file
     create_admin_secrets_file
     create_user_secrets_file "${OPENSLIDES_USER_FIRSTNAME}" "${OPENSLIDES_USER_LASTNAME}"
     update_nginx_config
