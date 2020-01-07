3bd7a9455ecae3e3cc94ff8460b31afea54f7e75
Gernot Schulz <gernot@intevation.de>
2020-01-07 16:56:29 +0100

    repmgr: Promote script that creates a base backup

---
+++ repmgr/repmgr-promote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is called as repmgr's promote_command which is configured in
+# /etc/repmgr.conf.
+
+BACKUP_DIR="/var/lib/postgresql/backup/"
+
+# Create base backup
+mkdir -p "$BACKUP_DIR"
+pg_basebackup -D - -Ft \
+  --wal-method=fetch --checkpoint=fast \
+  --write-recovery-conf \
+  --label="Base backup during repmgr promotion to primary" |
+gzip > "${BACKUP_DIR}/backup-$(date '+%F-%H:%M:%S').tar.bz2"
+
+# Promote node from standby to primary
+/usr/bin/repmgr standby promote -f /etc/repmgr.conf
