9e0241c521965c3f8f439e9d73a25a7c98d21ea3
Gernot Schulz <gernot@intevation.de>
2020-02-03 16:12:56 +0100

    Add script to start backup mode in all DB containers

---
+++ contrib/openslides-pg-backuptrigger.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This is a wrapper script that will activate Postgres' backup mode on
+# all OpenSlides database containers.  The containers are identified by
+# the label org.openslides.role=postgres.
+
+ME=$(basename -s .sh "${BASH_SOURCE[0]}")
+ACTION=
+ERRORS=
+
+usage() {
+cat <<EOF
+Usage: $ME <start|stop>
+
+Trigger Postgres's backup mode in all OpenSlides database containers.
+The containers are identified by the label org.openslides.role=postgres.
+EOF
+}
+
+case "$1" in
+  start)
+    ACTION=start ;;
+  stop)
+    ACTION=stop ;;
+  *)
+    usage
+    exit 2 ;;
+esac
+
+while read -r id name; do
+  printf "INFO: Backup mode %s on node %s (%s)...\n" "$ACTION" "$name" "$id"
+  docker exec "$id" pg_backuptrigger "$ACTION" || ERRORS=1
+  echo
+done < <(docker ps \
+  --filter label=org.openslides.role=postgres \
+  --format '{{.ID}}\t{{.Names}}')
+
+if [[ $ERRORS ]]; then
+  echo "WARNING: Finished but WITH ERRORS!"
+  exit 1
+fi
