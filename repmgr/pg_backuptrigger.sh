f17607d52ce0c303f71f33cb05c0de95f4397797
Gernot Schulz <gernot@intevation.de>
2020-02-04 15:16:31 +0100

    repmgr: Check for recovery mode before backup

--- repmgr/pg_backuptrigger.sh
+++ repmgr/pg_backuptrigger.sh
@@ -11,15 +11,27 @@ Usage: ${BASH_SOURCE[0]} <start|stop> [backup label]
 EOF
 }
 
+check_recovery() {
+  local RECOVERY
+  RECOVERY="$(psql -tAv ON_ERROR_STOP=1 \
+    <<< "SELECT pg_is_in_recovery();")"
+  if [[ "$RECOVERY" = "t" ]]; then
+    echo "INFO: recovery is in progress. Is this a standby node?"
+    return 1
+  fi
+}
+
 case $1 in
   "start")
     shift
+    check_recovery || exit 0
     LABEL=${1:-$DEFAULT_LABEL}
     psql -tAv ON_ERROR_STOP=1 -v label="$LABEL" <<< \
       "SELECT pg_start_backup(:'label')" &&
       cat 11/main/backup_label
     ;;
   "stop")
+    check_recovery || exit 0
     psql -tAv ON_ERROR_STOP=1 <<< "SELECT pg_stop_backup()"
     ;;
   *)
