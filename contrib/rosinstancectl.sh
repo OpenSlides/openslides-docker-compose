7bccfb35ab7f8bb56df63b8fb8f524513316c3b8
Gernot Schulz <gernot@intevation.de>
2020-02-12 16:23:28 +0100

    rosinstancectl: Adapt for --json mode

--- contrib/rosinstancectl.sh
+++ contrib/rosinstancectl.sh
@@ -17,4 +17,26 @@ readarray a < "$CONF"
 printf -v nodes ",%s" ${a[@]}
 
 [[ -n "$nodes" ]] || fatal "Nodes list is empty"
-exec clush -o "-ttq -o BatchMode=yes" -qS -b -w "${nodes:1}" osinstancectl $*
+
+# Test if --json output was requested
+#
+# XXX: This will fail CLI options such as "-nj" instead of "-n -j".  To fix
+# this, getopt should be used here as well.
+if grep -qwE -- "(--json|-j)" <<< "$*"; then
+    out="$(clush -o "-ttq -o BatchMode=yes" -qS -b -w "${nodes:1}" \
+            osinstancectl "$@" --color=never |
+        # Filter out clush headers (we do not use the -L/-N options to prevent
+        # clush from aggregating the output)
+        awk '/^-{15}$/ {h++; next;} h == 1 {next;} h == 2 {h=0;} 1')"
+    len="$(jq '. | length' <<< "$out")"
+    # Compare string because jq may return, e.g., '1 1'.  There is probably
+    # a better way to use jq.
+    if [[ "$len" = '1' ]]; then
+      jq <<< "$out" # simply print
+    else
+      # merge records from different sources
+      jq -s '{ instances: map(.instances[0]) }' <<< "$out"
+    fi
+else
+    exec clush -o "-ttq -o BatchMode=yes" -qS -b -w "${nodes:1}" osinstancectl "$@"
+fi
