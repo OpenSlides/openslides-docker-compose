476ff736ff649a3bb8807694f24fdaa4bf74c8d4
Gernot Schulz <gernot@intevation.de>
2020-08-13 09:56:15 +0200

    osinstancectl: Fix cloning of stacks
    When trying to find the database container to clone from, more than one
    result would be returned if there were older, stopped database
    containers.  This patch filters the output down to running containers of
    which there must only be one.

--- contrib/osinstancectl.sh
+++ contrib/osinstancectl.sh
@@ -784,7 +784,7 @@ clone_secrets() {
 containerid_from_service_name() {
   local id
   local cid
-  id="$(docker service ps -q "$1")"
+  id="$(docker service ps -q --filter desired-state=running "$1")"
   [[ -n "$id" ]] ||
     fatal "Service $1 not found.  Make sure it is running."
   cid="$(docker inspect -f '{{.Status.ContainerStatus.ContainerID}}' "${id}")"
